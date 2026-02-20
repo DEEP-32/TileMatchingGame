037eb9905c99ef156156c44f585df6c767a81026
Deepanshu Singh <anshu3012ep@gmail.com>
2026-02-20 22:09:28 +0530

    [ADD] tile handler to handle animations

---
+++ Assets/InGame/Scripts/Tile.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace TileMatching {
+    public class Tile : MonoBehaviour {
+        
+    }
+}
---
+++ Assets/InGame/Scripts/TileHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+namespace TileMatching {
+    public class TileHandler : MonoBehaviour {
+
+        [SerializeField] float totalDuration = 1f;
+        [SerializeField] float startDelay = .2f;
+        
+        public void AnimateTiles(List<Tile> tiles,Transform toTransform ,Action<Tile> onSingleTileReached = null, Action onComplete = null) {
+            if (tiles == null || tiles.Count == 0) {
+                return;
+            }
+
+            int remainingTiles = tiles.Count;
+            for (var i = 0; i < tiles.Count; i++) {
+                Tile tile = tiles[i];
+                float delay = i * startDelay;
+                
+                var posTween = tile.transform.DOMove(toTransform.position, totalDuration);
+                var rotTween = tile.transform.DORotateQuaternion(toTransform.rotation, totalDuration);
+                
+                Sequence sequence = DOTween.Sequence();
+                sequence.Append(posTween).Join(rotTween);
+                sequence.SetDelay(delay);
+                sequence.OnComplete(() => {
+                    onSingleTileReached?.Invoke(tile);
+                    remainingTiles--;
+                    if (remainingTiles == 0) onComplete?.Invoke();
+                });
+            }
+        }
+    }
+}
